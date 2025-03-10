b5fedcf309f26131e34d29c194c492e0dc852df1
Mar4eloni <marko.spasenoski@gmail.com>
2025-03-10 12:26:53 +0100

    init commit

---
+++ Source/FiveSteps/FiveSteps.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class FiveSteps : ModuleRules
+{
+	public FiveSteps(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "NavigationSystem", "AIModule", "Niagara", "EnhancedInput" });
+    }
+}
---
+++ Source/FiveSteps/FiveStepsGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FiveStepsGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AFiveStepsGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AFiveStepsGameMode();
+};
+
+
+
